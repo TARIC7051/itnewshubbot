c39ee601fe50ef23f815dbb60dda7ee2e57bfcea
TARIC7051 <artur.avdeev@yandex.ru>
2026-01-26 19:25:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 BOT_TOKEN = "8562721007:AAHys576YNRZbX_xHHobtjeo-jyg7hWGngA"
 
 # Этот адрес Bothost даст после создания бота
-WEBHOOK_HOST = "https://your-bot-domain.bothost.ru"
+WEBHOOK_HOST = "BOT_TOKEN = "8562721007:AAHys576YNRZbX_xHHobtjeo-jyg7hWGngA"
+WEBHOOK_HOST = "https://itnewshubbot.TARIC666.repl.co"  # ← URL Replit
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_URL = WEBHOOK_HOST + WEBHOOK_PATH
+"
 
 WEBHOOK_PATH = "/webhook"
+
 WEBHOOK_URL = WEBHOOK_HOST + WEBHOOK_PATH
