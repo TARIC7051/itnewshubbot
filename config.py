59625e9e9bfb2d49b9991829e3d22382ea608861
TARIC7051 <artur.avdeev@yandex.ru>
2026-02-09 11:42:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-BOT_TOKEN = "8562721007:AAHys576YNRZbX_xHHobtjeo-jyg7hWGngA"
+import os
+
+# Токен бота берётся из переменных окружения Replit
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_TOKEN = "8562721007:AAHys576YNRZbX_xHHobtjeo-jyg7hWGngA"
